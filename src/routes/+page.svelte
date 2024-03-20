72b916ba704be5a5eee8211bb9e44e203a71f545
luniya <proton.me@proton.me>
2024-03-20 12:17:34 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<style>
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    background: #333c43;
+    color: #D3C6AA;
+  }
+  body {
+    text-align: center;
+    font-size: 1.5rem;
+  }
+  .sep {
+    width: 95%;
+    margin-left: 2.5%;
+    align-self: center;
+    height: 1.5px;
+    background: #7FBBB3;
+  }
+  .container {
+    width: 100%;
+    display: flex;
+    gap: 3rem;
+    justify-content: center;
+  }
+  .available {
+    border: 2px solid #7FBBB3;
+    padding: 18px;
+    width: 600px;
+    text-align: left;
+  }
+  #plan {
+    width: 600px;
+
+    border: 2px solid #7FBBB3;
+  }
+</style>
+
+<html lang="english">
+  <head>
+  </head>
+    <body>
+      <h1>ticketree</h1>
+      welcome to ticketry <br> <br> 
+    <div class="sep"></div>
+
+    <br> <br>
+    <br>
+    <div class="container">
+      <div class="available">
+        seat 11.12 <br>
+        seat 11.13 <br>
+        seat 11.14 <br>
+        seat 11.15 <br>
+        seat 11.16 <br>
+      </div>
+      <div class="plan-venue"> <img id="plan" src="https://www.ccdewerf.be/files/download/zaalplan-schouwburg-2019-2020-website.jpg" alt=""></div>
+    </div>
+    </body>
+</html>
